8b5d904cdf138ad941a95fd5ebe640bbd7a87df6
Mario Martínez Vitutia <9205625@alumnos.ufv.es>
2023-11-06 21:18:30 +0100

    V1 finish

--- graviton.js
+++ graviton.js
@@ -1,10 +1,55 @@
-const gravitonElements = document.querySelectorAll('.graviton')
+const gravitonElements = document.querySelectorAll('.graviton');
 
-window.addEventListener('scroll', fun =>{
-    let scrollY = window.scrollY
+function calcGravity() {
+    let scrollY = window.scrollY;
     gravitonElements.forEach(element => {
-        let newPosition= element.dataset.gravity * 0.2 * scrollY + 'px';
-        element.style.marginTop = newPosition;
-        console.log()
+        let gravity;
+        console.log("Data-Gravity: " + element.dataset.gravity);
+        
+        switch (parseInt(element.dataset.gravity, 10)) {
+            case 1:
+                gravity = 0.1;
+                break;
+            case 2:
+                gravity = 0.2;
+                break;
+            case 3:
+                gravity = 0.3;
+                break;
+            case 4:
+                gravity = 0.4;
+                break;
+            case 5:
+                gravity = 0.5;
+                break;
+            case -1:
+                gravity = -0.1;
+                break;
+            case -2:
+                gravity = -0.2;
+                break;
+            case -3:
+                gravity = -0.3;
+                break;
+            case -4:
+                gravity = -0.4;
+                break;
+            case -5:
+                gravity = -0.5;
+                break;
+            default:
+                gravity = 0.3;
+                break;
+        }
+
+        console.log("Gravity: " + gravity);
+        let translateY = gravity * scrollY;
+        element.style.transform = `translateY(${translateY}px)`;
+        console.log("TranslateY: " + translateY);
     });
-})
+    requestAnimationFrame(calcGravity);
+}
+
+calcGravity();
+
+
